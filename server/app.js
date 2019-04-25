2be643ce6c856981ff9deb0bad869233c6f38426
Seungje Jun <ninanung@naver.com>
2019-04-25 16:39:31 +0900

    server and frontend setting

--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ let http = require('http');
 let express = require('express');
 let app = express();
 app.use(express.urlencoded());
-
 let mongoose = require('mongoose');
 
 const port = 3000;
+
+const server = http.createServer(app);
+server.listen(port, function() {
+    console.log(`server running in ${port}`);    
+});
