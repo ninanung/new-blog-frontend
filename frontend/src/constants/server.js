e152c4cb6b3b2dfdcf73568a2b62d6c6a5894114
Seungje Jun <ninanung@naver.com>
2019-05-20 18:03:09 +0900

    frontend

--- frontend/src/constants/server.js
+++ frontend/src/constants/server.js
@@ -1,8 +1,8 @@
 const SERVER_URL = 'http://localhost:3000';
 
 export const CREATE_POST = `${SERVER_URL}/post/post/create`;
-export const LOGIN = `${SERVER_URL}/get/login`;
+export const LOGIN = `${SERVER_URL}/login/get`;
 export const DELETE_POST = `${SERVER_URL}/post/post/delete`;
 export const EDIT_POST = `${SERVER_URL}/post/post/edit`;
-export const GET_POSTS = `${SERVER_URL}/get/posts`;
-export const EDIT_PROFILE = `${SERVER_URL}/post/profile/edit`;
+export const GET_POSTS = `${SERVER_URL}/posts/get`;
+export const EDIT_PROFILE = `${SERVER_URL}/profile/post/edit`;
