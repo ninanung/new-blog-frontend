a3f99747b77093d6206b9f9490756944db593e08
Seungje Jun <ninanung@naver.com>
2019-07-19 18:29:18 +0900

    add linkedin icon and url

--- frontend/src/screens/footer/footer.jsx
+++ frontend/src/screens/footer/footer.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './footer.css';
 
 import github_icon from '../../assets/github_icon.png';
+import linkedin_icon from '../../assets/linkedin_icon.png';
 
 class Footer extends React.Component {
     render() {
         return (
             <div>
+                <a className='footer_link' href='https://www.linkedin.com/in/seungje-jun-414a8718b/'><img className='footer_github_icon' alt='linklinklink in' src={linkedin_icon} /></a>
                 <a className='footer_link' href='https://github.com/ninanung'><img className='footer_github_icon' alt='gitgitgit hub' src={github_icon} /></a>
                 <a className='footer_profile' href='/profile'>Profile</a>
             </div>
